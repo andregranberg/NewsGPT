97dcab6eddc000ba4a6975e669328143e76d6453
andregranberg <38399597+andregranberg@users.noreply.github.com>
2023-11-09 01:22:54 +0100

    Update utils.py
    replaced summarize_article api to working one

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import requests
 
 def fetch_first_article_url(keyword: str, RapidAPIKey: str) -> str | None:
     url = "https://bing-news-search1.p.rapidapi.com/news/search"
-    querystring = {"q": keyword, "textFormat": "Raw", "safeSearch": "Off"}
+    querystring = {"q": keyword, "textFormat": "Raw", "safeSearch": "Off", "mkt":"en-US"}
     headers = {
         "X-BingApis-SDK": "true",
         "X-RapidAPI-Key": RapidAPIKey,
@@ -42,24 +42,32 @@ def extract_article_text(article_url: str, RapidAPIKey: str) -> str:
 
 
 def summarize_article(news_article: str, RapidAPIKey: str) -> str:
-    url = "https://open-ai21.p.rapidapi.com/conversationgpt"
+    url = "https://open-ai21.p.rapidapi.com/conversationgpt35"
     payload = {
         "messages": [
             {
                 "role": "user",
-                "content": f"rewrite this to remove repetetive stuff while keeping the key information: {news_article}",
+                "content": f"give me a short summary of this text in journalistic style: {news_article}"
             }
-        ]
+        ],
+        "web_access": False,
+        "system_prompt": "",
+        "temperature": 0.9,
+        "top_k": 10,
+        "top_p": 0.9,
+        "max_tokens": 256
     }
     headers = {
         "content-type": "application/json",
-        "X-RapidAPI-Key": RapidAPIKey,
-        "X-RapidAPI-Host": "open-ai21.p.rapidapi.com",
+        "X-RapidAPI-Key": RapidAPIKey,  # Ensure you use the correct API key variable or config
+        "X-RapidAPI-Host": "open-ai21.p.rapidapi.com"
     }
+
     response = requests.post(url, json=payload, headers=headers)
-    gpt_response = response.json()["GPT"]
-    parsed_gpt_response = json.loads(gpt_response)
+    response_json = response.json()
 
-    final_response = json.loads(parsed_gpt_response)
+    if response_json.get('status') and 'result' in response_json:
+        return response_json['result']
+    else:
+        return "Error: Could not summarize the article."
 
-    return final_response
